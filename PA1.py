4cef6feb8c9e84a63ba8c75c3680b586950bf505
Riley H <rharwood17@gmail.com>
2023-09-12 17:25:08 -0400

    Setting up argparse and input reader

---
+++ PA1.py
@@ -0,0 +1,16 @@
+"""
+Author: Riley Harwood
+Last Edit: 12 September 2023
+The PA1.py module contains the __ functions to meet the objectives of PA1 for 605.621.
+"""
+from typing import TextIO
+
+
+def process_files(inputFile: TextIO, outputFile: TextIO) -> None:
+    """
+
+    :param inputFile:
+    :param outputFile:
+    :return:
+    """
+    next_line = inputFile.readline()
